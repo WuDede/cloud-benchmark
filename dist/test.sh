f091800458f59f08639557b8519913148e31d987
WuDede <lingxve886@163.com>
2018-04-11 17:39:45 +0800

    dede: fix bugs

--- dist/test.sh
+++ dist/test.sh
@@ -24,7 +24,7 @@ test_unixbench()
 	do 
 		[ -f $RUN_FLAG ] || return
 		ts=$(awk '{print $1}' /proc/uptime)
-        msg_warn "./Run -q -c $NR_CPU -i 1 system"
+        echo "./Run -q -c $NR_CPU -i 1 system"
 		./Run -q -c $NR_CPU -i 1 system 2>&1 | tee -a $logfile
 		te=$(awk '{print $1}' /proc/uptime)
 		tc=$(echo $ts $te | awk '{print $2 - $1}')
@@ -60,7 +60,7 @@ test_y_cruncher()
 	do 
 		[ -f $RUN_FLAG ] || return
 		ts=$(awk '{print $1}' /proc/uptime)
-        msg_warn "./y-cruncher bench $pi_bit"
+        echo "./y-cruncher bench $pi_bit"
 		./y-cruncher bench $pi_bit 2>&1 | tee -a $logfile
 		te=$(awk '{print $1}' /proc/uptime)
 		tc=$(echo $ts $te | awk '{print $2 - $1}')
@@ -76,7 +76,7 @@ test_sysbench_fileio()
 	local rwratio=1
 	./sysbench --time=$timeout fileio --file-block-size=$blksize --file-test-mode=$1 --file-rw-ratio=$rwratio prepare 1>&2
 	echo -n "sysbench timeout=$timeout blksize=$blksize rwratio=$rwratio testmode=$1 "
-	msg_warn "./sysbench --time=$timeout fileio --file-block-size=$blksize --file-test-mode=$1 --file-rw-ratio=$rwratio run" 1>&2
+	echo "./sysbench --time=$timeout fileio --file-block-size=$blksize --file-test-mode=$1 --file-rw-ratio=$rwratio run" 1>&2
 	./sysbench --time=$timeout fileio --file-block-size=$blksize --file-test-mode=$1 --file-rw-ratio=$rwratio run 2>&1 | grep "read, MiB\|written, MiB" | tr '\n' ' ' | sed "s|[[:blank:]]\+| |g"
 	echo ""
 	./sysbench --time=$timeout fileio --file-block-size=$blksize --file-test-mode=$1 --file-rw-ratio=$rwratio cleanup 1>&2
@@ -89,7 +89,7 @@ test_sysbench_memory()
 	local oper=$2
 	local mode=$3
 	echo -n "sysbench timeout=$timeout blksize=$blksize oper=$oper mode=$mode "
-    msg_warn "./sysbench --time=$timeout --threads=$NR_CPU --memory-block-size=$blksize memory --memory-total-size=4096G --memory-oper=$oper --memory-access-mode=$mode run" 1>&2
+    echo "./sysbench --time=$timeout --threads=$NR_CPU --memory-block-size=$blksize memory --memory-total-size=4096G --memory-oper=$oper --memory-access-mode=$mode run" 1>&2
 	./sysbench --time=$timeout --threads=$NR_CPU --memory-block-size=$blksize memory --memory-total-size=4096G --memory-oper=$oper --memory-access-mode=$mode run 2>&1 | grep "MiB transferred"
 }
 
@@ -112,7 +112,7 @@ test_sysbench()
 			test_sysbench_fileio $fileop | tee -a $logfile
 		done
 
-        msg_warn "./sysbench --time=30 --threads=$NR_CPU cpu run"
+        echo "./sysbench --time=30 --threads=$NR_CPU cpu run"
 		echo -n "timeout=30 threads=$NR_CPU " 2>&1 | tee -a $logfile
 		./sysbench --time=30 --threads=$NR_CPU cpu run 2>&1 | grep "events per second:" 2>&1 | tee -a $logfile
 
@@ -127,7 +127,7 @@ test_sysbench()
 
 		for trd_times in 1 2 4 8 16 32 64 128 256
 		do 
-            msg_warn "./sysbench --time=30 --threads=$(( NR_CPU * trd_times )) threads run"
+            echo "./sysbench --time=30 --threads=$(( NR_CPU * trd_times )) threads run"
 			echo -n "timeout=30 threads=$(( NR_CPU * trd_times )) " 2>&1 | tee -a $logfile
 			./sysbench --time=30 --threads=$(( NR_CPU * trd_times )) threads run 2>&1 | grep "total number of events:" 2>&1 | tee -a $logfile
 		done 
@@ -159,19 +159,19 @@ test_qperf()
 	echo "myip=$myip reip=$reip vm1=$vm1 vm2=$vm2" | tee -a $logfile
 	[ -z "$myip" -o -z "$vm1" -o -z "$vm2" -o -z "$reip" ] && return 1
 	#mark that we are ok, if file ${RUN_FLAG}.qperf exist, means $reip ready
-    msg_warn "[ssh $SSH_OPT root@$reip touch ${RUN_FLAG}.qperf]"
+    echo "[ssh $SSH_OPT root@$reip touch ${RUN_FLAG}.qperf]"
 	ssh $SSH_OPT root@$reip touch ${RUN_FLAG}.qperf
 	#start server
-    msg_warn "[start qperf server]"
+    echo "[start qperf server]"
 	qperf 2>&1 > ${logfile}.qperf-server &
 	#wait the other vm ready
-    msg_warn "wait the other vm ready"
+    echo "wait the other vm ready"
 	while [ -f ${RUN_FLAG} -a -f ${RUN_FLAG}.qperf ]
 	do
 		sleep 3
 	done
 	# vm2 wait vm1 test ok
-    msg_warn "vm2 wait vm1 test ok"
+    echo "vm2 wait vm1 test ok"
 	if [ "$myip" = "$vm2" ]; then 
 		#vm1 test ok then create file ${RUN_FLAG}.qperf.step1
 		while [ ! -f ${RUN_FLAG}.qperf.step1 ] && [ -f ${RUN_FLAG} ]
@@ -183,25 +183,25 @@ test_qperf()
 	for i in $(seq $nr_iter)
 	do 
 		[ -f ${RUN_FLAG} ] || break
-        msg_warn "do test [qperf $reip -t 10 -oo msg_size:1:64K:*2 -vu sctp_lat tcp_lat udp_lat sctp_bw tcp_bw udp_bw]"
+        echo "do test [qperf $reip -t 10 -oo msg_size:1:64K:*2 -vu sctp_lat tcp_lat udp_lat sctp_bw tcp_bw udp_bw]"
 		qperf $reip -t 10 -oo msg_size:1:64K:*2 -vu sctp_lat tcp_lat udp_lat sctp_bw tcp_bw udp_bw 2>&1 | tee -a $logfile
 	done 
 	#vm1 wait vm2 test ok
 	if [ "$myip" = "$vm1" ]; then 
 		rm -rf ${RUN_FLAG}.qperf.step1
 		touch ${RUN_FLAG}.qperf.step1
-        msg_warn "touch ${RUN_FLAG}.qperf.step1"
-        msg_warn "ssh $SSH_OPT root@$reip rm -rf ${RUN_FLAG}.qperf.step1; touch ${RUN_FLAG}.qperf.step1"
+        echo "touch ${RUN_FLAG}.qperf.step1"
+        echo "ssh $SSH_OPT root@$reip rm -rf ${RUN_FLAG}.qperf.step1; touch ${RUN_FLAG}.qperf.step1"
 		ssh $SSH_OPT root@$reip "rm -rf ${RUN_FLAG}.qperf.step1; touch ${RUN_FLAG}.qperf.step1"
 	else 
 		rm -rf ${RUN_FLAG}.qperf.step2
 		touch ${RUN_FLAG}.qperf.step2
-        msg_warn "touch ${RUN_FLAG}.qperf.step2"
-        msg_warn "ssh $SSH_OPT root@$reip rm -rf ${RUN_FLAG}.qperf.step2; touch ${RUN_FLAG}.qperf.step2"
+        echo "touch ${RUN_FLAG}.qperf.step2"
+        echo "ssh $SSH_OPT root@$reip rm -rf ${RUN_FLAG}.qperf.step2; touch ${RUN_FLAG}.qperf.step2"
 		ssh $SSH_OPT root@$reip "rm -rf ${RUN_FLAG}.qperf.step2; touch ${RUN_FLAG}.qperf.step2"
 	fi 
 	#wait all test ok
-    msg_warn "wait all test ok"
+    echo "wait all test ok"
 	while [ -f ${RUN_FLAG} ]
 	do
 		if [ -f ${RUN_FLAG}.qperf.step1 -a -f ${RUN_FLAG}.qperf.step2 ]; then 
@@ -218,8 +218,8 @@ test_qperf()
 
 do_test()
 {
-	test_unixbench $NR_UNIXBENCH
-	test_y_cruncher $NR_Y_CRUNCHER
+	#test_unixbench $NR_UNIXBENCH
+	#test_y_cruncher $NR_Y_CRUNCHER
 	test_sysbench $NR_SYSBENCH
 	
 	#test_qperf $NR_QPERF
